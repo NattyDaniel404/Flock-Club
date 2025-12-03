4fb42f26bd29e8971245a11a1cc552dae82d65b5
NattyDaniel404 <nattydaniel404@gmail.com>
2025-12-02 20:48:42 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'public')));
 
 const users = new Map();    // id -> {id,name,x,y,look}
 const tttGames = new Map(); // roomId -> { X, O, board }
