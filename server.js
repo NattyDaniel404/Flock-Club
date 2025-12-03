0ed85606150bd79d840019507827eff89ced7039
NattyDaniel404 <nattydaniel404@gmail.com>
2025-12-02 20:34:27 -0500

    updated files

--- server.js
+++ server.js
@@ -185,3 +185,5 @@ io.on('connection', (socket)=>{
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, ()=> console.log('Server listening on http://localhost:'+PORT));
+
+app.use(express.static("public"));
